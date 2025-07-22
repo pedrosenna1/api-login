0afb7c387594b7571d9df1da6bb089a3f938247c
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-07-21 21:04:44 -0300

    Reverte alterações indevidas no teste de recuperação de senha

--- tests/TS02 - recuperaSenha.test.js
+++ tests/TS02 - recuperaSenha.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 const app = require('../src/server');
 
-describe('Solicitar Recuperação de senha - /api/auth/forgot-password', () => {
+describe('Recuperação de Senha - /api/auth/forgot-password', () => {
   const baseUrl = '/api/auth';
   const testUser = {
     email: 'reginaldo@exemplo.com',
