3b7bc8f885e804a11a602a02b83de22ac94f611a
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-07-22 11:48:29 -0300

    validação do restante dos endpoints TS01-criarUsuario

--- tests/TS01-criarUsuario.test.js
+++ tests/TS01-criarUsuario.test.js
@@ -1,16 +1,14 @@
 const request = require('supertest')
 const {expect} = require('chai')
+const app = require('../src/server')
+const login = require('../fixtures/login.json')
 
 describe('validar usuario',()=>{
     let email
     before (async() => {
-        const response = await request('http://localhost:3001')
+        const response = await request(app)
             .post('/api/auth/register')
-            .send({
-                email: "reginaldo@exemplo.com",
-                password: "senha123",
-                name: "Reginaldo"
-            })
+            .send(login)
             if(response.body.error == 'Email já cadastrado'){
                 expect(response.status).to.eq(409)
                 email = 'reginaldo@exemplo.com'
@@ -23,8 +21,8 @@ describe('validar usuario',()=>{
 
 })
     
-    it('validar usuario',async()=>{
-        const response = await request('http://localhost:3001')
+    it('validar usuario (status 200)',async()=>{
+        const response = await request(app)
             .get(`/api/auth/status/${email}`)
         
         expect(response.status).to.eq(200)
@@ -32,6 +30,46 @@ describe('validar usuario',()=>{
 
     })
 
+    it('entrada com number ao invés de string no input nome (status 400)', async () =>{
+        const response = await request(app)
+            .post('/api/auth/register')
+            .send({...login,name: 1234})
+            expect(response.status).to.eq(400)
+            expect(response.body.message).to.eq('Nome deve ser uma string não vazia')
+
+
+
+    } )
+    
+    it('criar usuario com email sem dominio (status 400)', async () =>{
+        const response = await request(app)
+            .post('/api/auth/register')
+            .send({...login,email: 'reginaldo'})
+            expect(response.status).to.eq(400)
+            expect(response.body.message).to.eq('Formato de email inválido')
+            
+    } )
+
+    it('criar usuario com campo de senha com string vazia (status 400)', async () =>{
+        const response = await request(app)
+            .post('/api/auth/register')
+            .send({...login,email: 'pedro@gmail.com.br', password: ''})
+            expect(response.status).to.eq(400)
+            expect(response.body.message).to.eq('Email, senha e nome são obrigatórios')
+            
+            
+    } )
+
+    it('criar usuario com email ja cadastrado (status 409)', async () =>{
+        const response = await request(app)
+            .post('/api/auth/register')
+            .send(login)
+            expect(response.status).to.eq(409)
+            expect(response.body.message).to.eq('Email já cadastrado')
+            
+            
+            
+    } )
 
 
 })
